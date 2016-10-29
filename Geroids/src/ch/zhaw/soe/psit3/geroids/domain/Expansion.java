4b2dced298a098d737ca97ee18ff6b03cb27bde4
kadermat <kadermat@students.zhaw.ch>
2016-10-29 15:45:33 +0200

    added class Expansiojn

--- Geroids/src/ch/zhaw/soe/psit3/geroids/domain/Expansion.java
+++ Geroids/src/ch/zhaw/soe/psit3/geroids/domain/Expansion.java
@@ -1,5 +1,36 @@
 package ch.zhaw.soe.psit3.geroids.domain;
-
+/*
+ * Defines the Expansion of the body relatively to his Position point;
+ * For Prototype just square Expansions
+ * 
+ */
 public class Expansion {
+	private int xExpansion;
+	private int yExpansion;
+	
+	public Expansion(int xExpansion,int yExpansion){
+		this.xExpansion = xExpansion;
+		this.yExpansion = yExpansion;
+	}
+	
+	public Expansion(int sqaresize){
+		this.xExpansion = sqaresize;
+		this.yExpansion = sqaresize;
+	}
+	
+	public int getxExpansion() {
+		return xExpansion;
+	}
+	public void setxExpansion(int xExpansion) {
+		this.xExpansion = xExpansion;
+	}
+	public int getyExpansion() {
+		return yExpansion;
+	}
+	public void setyExpansion(int yExpansion) {
+		this.yExpansion = yExpansion;
+	}
+	
+	
 
 }
