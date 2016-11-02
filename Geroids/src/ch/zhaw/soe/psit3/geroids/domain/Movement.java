70685af2c5158d2ec47cdcf1e3e6cff3dc99f0bd
kadermat <kadermat@students.zhaw.ch>
2016-11-02 08:02:28 +0100

    added javadoc

--- Geroids/src/ch/zhaw/soe/psit3/geroids/domain/Movement.java
+++ Geroids/src/ch/zhaw/soe/psit3/geroids/domain/Movement.java
@@ -5,6 +5,18 @@ public class Movement {
 	private int xSpeed;
 	private int ySpeed;
 
+	/**
+	 * Creates Movement Object with two directional speeds
+	 * 
+	 * @param xSpeed Speed in x direction
+	 * @param ySpeed Speed in y direction
+	 */
+	public Movement(int xSpeed, int ySpeed) {
+		super();
+		this.xSpeed = xSpeed;
+		this.ySpeed = ySpeed;
+	}
+
 	public void setxSpeed(int xSpeed) {
 		this.xSpeed = xSpeed;
 	}
@@ -21,10 +33,18 @@ public class Movement {
 		return ySpeed;
 	}
 
+	/**
+	 * Increases the X Speed by the given amount
+	 * @param x The amount of Speed to increase the current ySpeed
+	 */
 	public void increaseXSpeed(int x) {
 		this.xSpeed = xSpeed + x;
 	}
 
+	/**
+	 * Increases the XY Speed by the given amount
+	 * @param y The amount of Speed to increase the current ySpeed
+	 */
 	public void increaseYSpeed(int y) {
 		this.ySpeed = ySpeed + y;
 	}
