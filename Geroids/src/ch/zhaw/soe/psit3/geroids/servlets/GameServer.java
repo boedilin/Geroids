cf7cbe550e8a397061f58f7bfe9edddcba124372
Valentin Bossi <bossival@students.zhaw.ch>
2016-11-03 10:04:07 +0100

    refactoring FileServer and renamed to GameServer

---
+++ Geroids/src/ch/zhaw/soe/psit3/geroids/servlets/GameServer.java
@@ -0,0 +1,45 @@
+package ch.zhaw.soe.psit3.geroids.servlets;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+
+import org.eclipse.jetty.websocket.server.WebSocketHandler;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+
+public class GameServer
+{
+    public static void main( String[] args ) throws Exception
+    {
+        Server server = new Server(8080);
+
+        WebSocketHandler wsHandler = new WebSocketHandler() {
+			@Override
+			public void configure(WebSocketServletFactory factory) {
+				factory.register(MyWebSocketHandler.class);
+			}
+		};
+
+        ServletContextHandler context = new ServletContextHandler(
+                ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        context.setResourceBase("./public");
+        
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] { wsHandler, context, new DefaultHandler() });
+        server.setHandler(handlers);
+
+        // Add dump servlet
+        context.addServlet(DumpServlet.class, "/dump/*");
+        // Add default servlet
+        context.addServlet(DefaultServlet.class, "/");
+
+        server.start();
+        server.join();
+    }
+}
