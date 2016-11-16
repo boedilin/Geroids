5fc6eacb779bb3aa29cc4811f7bad081af6919ab
kadermat <kadermat@students.zhaw.ch>
2016-11-16 19:26:30 +0100

    updated javadoc for public methods

--- Geroids/src/ch/zhaw/soe/psit3/geroids/domain/Figure.java
+++ Geroids/src/ch/zhaw/soe/psit3/geroids/domain/Figure.java
@@ -8,7 +8,7 @@ public class Figure {
 	private Position position;
 	private Skin skin;
 	private Type type;
-	private final Movement MOVEMENT_FOR_PROJECTILE = new Movement(0,-10);
+	private static final Movement MOVEMENT_FOR_PROJECTILE = new Movement(0,-10);
 
 	public Figure(Type type, Skin skin, int x, int y){
 		this.skin = skin;
@@ -20,14 +20,26 @@ public class Figure {
 		this.position = position;
 	}
 	
+	/**
+	 * Moves the figure to the Left
+	 * @param amount Amount of pixels the figure should move to the left
+	 */
 	public void moveLeft(int amount){
 		this.position.setxCoordiante(this.position.getxCoordiante()-amount);
 	}
 	
+	/**
+	 * Moves the figure to the right
+	 * @param amount Amount of pixels the figure should move to the right
+	 */
 	public void moveRight(int amount){
 		this.position.setxCoordiante(this.position.getxCoordiante()+amount);
 	}
 	
+	/**
+	 * Shoots a projectile out of the top middle of the figure. The projectile has a y-Speed of -10 pixel/tick
+	 * @return the projectile that has been shoot
+	 */
 	public Projectile shoot(){
 		
 		int xPosInMiddleOfFigure = this.position.getxCoordiante() + this.position.getxLength()/2;
@@ -38,6 +50,10 @@ public class Figure {
 		
 	}
 	
+	/**
+	 * Translates the Object inclusive the position attribute into a JSONObject
+	 * @return JSONObject of figure
+	 */
 	@SuppressWarnings("unchecked")
 	public JSONObject toJSONObject(){
 		JSONObject obj = new JSONObject();
