c28c28d5284902fdc1dc9c1bc279ab6c2160e246
Linda Boedi <boedilin@students.zhaw.ch>
2016-12-01 10:30:12 +0100

    added TestPosition

---
+++ Geroids/src/ch/zhaw/soe/psit3/geroids/test/unit/TestPosition.java
@@ -0,0 +1,14 @@
+package ch.zhaw.soe.psit3.geroids.test.unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPosition {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
